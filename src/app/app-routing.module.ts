b7bd28b22f9024141d1a774e0c10bf4127a884d7
Tricelle <tricellegray@gmail.com>
2019-10-01 16:04:47 -0700

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CvComponent } from './cv/cv.component';
+import { HomeComponent } from './home/home.component';
+import { ProjectPageComponent } from './project-page/project-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'cv', component: CvComponent},
+  {path: '', component: HomeComponent},
+  {path: 'project-page', component: ProjectPageComponent},
+  {path: '**',  component: HomeComponent, redirectTo: ''}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CvComponent } from './cv/cv.component';
+import { ProjectPageComponent } from './project-page/project-page.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CvComponent,
+    ProjectPageComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
